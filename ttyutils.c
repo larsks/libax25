larsks/libax25

commit 25619cc79e58a68bf88381a75ecdf93a9b8f9c90

ttyutils.c: added support for higher baud rates.
Signed-off-by: John Paul Morrison<jmorrison@bogomips.com>

ttyutils.c
@@ -33,6 +33,39 @@ static struct speed_struct {
 #ifdef  B460800
 	{460800,	B460800},
 #endif
+#ifdef  B500000
+	{500000,	B500000},
+#endif
+#ifdef  B576000
+	{576000,	B576000},
+#endif
+#ifdef  B921600
+	{921600,	B921600},
+#endif
+#ifdef  B1000000
+	{1000000,	B1000000},
+#endif
+#ifdef  B1152000
+	{1152000,	B1152000},
+#endif
+#ifdef  B1500000
+	{1500000,	B1500000},
+#endif
+#ifdef  B2000000
+	{2000000,	B2000000},
+#endif
+#ifdef  B2500000
+	{2500000,	B2500000},
+#endif
+#ifdef  B3000000
+	{3000000,	B3000000},
+#endif
+#ifdef  B3500000
+	{1000000,	B3500000},
+#endif
+#ifdef  B4000000
+	{4000000,	B4000000},
+#endif
 	{-1,		B0}
 };
 

commit 1ce8d1e7166c9e7fff65742bbaad8817f7f445f0

Fixed typo in B3500000 declaration, introduced by commit 25619cc79e58a68bf88381a75ecdf93a9b8f9c90
Impact: these new very high speed baudrate may not have been needed
in the past.

Signed-off-by: Nate Bargmann <n0nb@n0nb.us>
Signed-off-by: Thomas Osterried <ax25@x-berg.in-berlin.de>

ttyutils.c
@@ -61,7 +61,7 @@ static struct speed_struct {
 	{3000000,	B3000000},
 #endif
 #ifdef  B3500000
-	{1000000,	B3500000},
+	{B3500000,	B3500000},
 #endif
 #ifdef  B4000000
 	{4000000,	B4000000},